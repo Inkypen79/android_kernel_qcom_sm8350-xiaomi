a6e1d2dd1311d8ee6617891c6215b627b01acd54
Tommaso Fonda <fondatommaso2@gmail.com>
2025-01-06 21:23:26 +0100

    power: supply: qti_battery_charger_odin: Implement charging_enabled node
    Change-Id: Id927c07bab305a63665edfe44228051d76951933

--- drivers/power/supply/qti_battery_charger_odin.c
+++ drivers/power/supply/qti_battery_charger_odin.c
@@ -5270,6 +5270,55 @@ static ssize_t fg_vendor_show(struct class *c,
 }
 static CLASS_ATTR_RO(fg_vendor);
 
+static ssize_t charging_enabled_store(struct class *c,
+				      struct class_attribute *attr,
+				      const char *buf, size_t count)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	int rc;
+	bool val;
+
+	if (kstrtobool(buf, &val))
+		return -EINVAL;
+
+	if (val) {
+		/*
+		 * Enable charging, i.e. set the restricted current back to
+		 * its default value and unset the restriction boolean flag.
+		 */
+		rc = __battery_psy_set_charge_current(bcdev,
+				DEFAULT_RESTRICT_FCC_UA);
+		if (rc < 0)
+			return rc;
+		bcdev->restrict_fcc_ua = DEFAULT_RESTRICT_FCC_UA;
+		bcdev->restrict_chg_en = 0;
+	} else {
+		/*
+		 * Disable charging, i.e. set the restricted current to zero
+		 * and set the restriction boolean flag.
+		 */
+		rc = __battery_psy_set_charge_current(bcdev, 0 /* 0 uA */);
+		if (rc < 0)
+			return rc;
+		bcdev->restrict_fcc_ua = 0;
+		bcdev->restrict_chg_en = 1;
+	}
+
+	return count;
+}
+
+static ssize_t charging_enabled_show(struct class *c,
+				     struct class_attribute *attr, char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	bool val = !bcdev->restrict_chg_en && bcdev->restrict_fcc_ua;
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", val);
+}
+static CLASS_ATTR_RW(charging_enabled);
+
 static struct attribute *battery_class_attrs[] = {
 	&class_attr_soh.attr,
 	&class_attr_resistance.attr,
@@ -5412,6 +5461,7 @@ static struct attribute *battery_class_attrs[] = {
 	&class_attr_cell1_volt.attr,
 	&class_attr_cell2_volt.attr,
 	&class_attr_fg_vendor.attr,
+	&class_attr_charging_enabled.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(battery_class);
